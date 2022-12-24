9aebd261aef12c26801fa749e1bea1f998bc138c
Shekhar Banerjee <banerjeehacksgh007@gmail.com>
2022-12-24 20:58:46 +0530

    Add files via upload
    Initializing App Attempt 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# spam_classifier
+ Spam classifier 
---
+++ app.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+from nltk.tokenize import word_tokenize
+
+pipe = pickle.load(open('pipe.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+stop_words = set(stopwords.words('english'))
+
+ps = PorterStemmer()
+
+def basic_clean(x) :
+    words = word_tokenize(x.lower())
+    final = []
+    
+    
+    for word in words :     
+        if word.isalnum():
+            final.append(word)
+    
+
+    words = final[:]
+    final= []
+    
+    for i in words :
+        if i not in stop_words and i not in string.punctuation :
+            final.append(i)
+    words = final[:]
+    final= []
+    
+    for i in words :
+        final.append(ps.stem(i))
+    words = final[:]
+    final= []
+    return ' '.join(words)
+
+
+st.title("Email/SMS Spam Classifier")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = basic_clean(input_sms)
+    print(transformed_sms)
+    # 2. vectorize
+    vector_input = pipe.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    print(result)
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
